fd16a4d7a968bd9719057e81cc2d94b245b81aff
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-06-23 10:39:54 +0530

    Add controllers for for CRUD operations

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all.order(created_at: :desc)
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      redirect_to tasks_path, notice: 'Task created successfully!'
+    else
+      @tasks = Task.all.order(created_at: :desc)
+      render :index
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'Task updated successfully!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'Task was successfully deleted.'
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
+  end
+end
